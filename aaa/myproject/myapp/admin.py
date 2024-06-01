47ef7b57565043cba782d60bba798ee660575de1
ahmetygtozay <92581807+ahmetygtozay@users.noreply.github.com>
2024-06-01 12:47:06 +0300

    Delete aaa directory

--- aaa/myproject/myapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Conference, PaperSubmission, Review, Submission, Reviewer
-
-admin.site.register(Conference)
-admin.site.register(PaperSubmission)
-admin.site.register(Review)
-admin.site.register(Submission)
-admin.site.register(Reviewer)
